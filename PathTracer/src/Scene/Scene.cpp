ff7857febe1e96a6c06a58bf2eb5e77b6e7139f1
alexeyka159 <alexeyka07@mail.ru>
2022-03-01 22:22:23 +0400

    Comments deleted

--- PathTracer/src/Scene/Scene.cpp
+++ PathTracer/src/Scene/Scene.cpp
@@ -6,44 +6,6 @@
 
 Scene::Scene()
 {
-	//struct MeshComponent
-	//{
-	//	bool Data;
-	//	MeshComponent() = default;
-	//};
-	//struct TransformComponent
-	//{
-	//	glm::mat4 Transform;
-
-	//	TransformComponent() = default;
-	//	TransformComponent(const TransformComponent&) = default;
-	//	TransformComponent(const glm::mat4& transform)
-	//		: Transform(transform) {}
-
-	//	operator glm::mat4& () { return Transform; }
-	//	operator const glm::mat4& () const { return Transform; }
-	//};
-
-
-	//entt::entity entity = m_Registry.create();
-	//m_Registry.emplace<TransformComponent>(entity, glm::mat4(1.0f));
-
-	//if(m_Registry.any_of<TransformComponent>(entity))
-	//	TransformComponent& transform = m_Registry.get<TransformComponent>(entity);
-
-	//auto view = m_Registry.view<TransformComponent>();
-	//for (auto entity : view)
-	//{
-	//	//view.get<TransformComponent>(entity);
-	//	TransformComponent& transform = m_Registry.get<TransformComponent>(entity);
-	//}
-
-	//auto group = m_Registry.group<TransformComponent>(entt::get<MeshComponent>);
-	//for (auto entity : group)
-	//{
-	//	//auto& [a, b] = { 1, 2 };
-	//	//auto gr = group.get<TransformComponent, MeshComponent>(entity);
-	//}
 }
 
 Scene::~Scene()
